76b094d19f6ccaa08d709da26d1642ed55eaf28d
UppariAkshay <uppari.akshay@gmail.com>
2024-12-27 22:38:57 +0530

    errors removed

--- src/App.js
+++ src/App.js
@@ -1,52 +1,44 @@
-import React from 'react';
+import React from "react";
 import ReactFlow, {
-  ReactFlowProvider,
   addEdge,
-  MiniMap,
+  useNodesState,
+  useEdgesState,
+  Background,
   Controls,
-  Background
-} from 'react-flow-renderer';
-import { useState } from 'react';
-import './App.css';
+  MiniMap,
+} from "reactflow";
+import "reactflow/dist/style.css";
 
 const initialNodes = [
-  { id: '1', type: 'input', data: { label: 'Start Node' }, position: { x: 250, y: 0 } },
-  { id: '2', data: { label: 'Processing Node' }, position: { x: 250, y: 150 } },
-  { id: '3', type: 'output', data: { label: 'End Node' }, position: { x: 250, y: 300 } },
+  { id: "1", type: "input", data: { label: "Input Node" }, position: { x: 250, y: 0 } },
+  { id: "2", data: { label: "Default Node" }, position: { x: 250, y: 150 } },
+  { id: "3", type: "output", data: { label: "Output Node" }, position: { x: 250, y: 300 } },
 ];
 
-const initialEdges = [
-  { id: 'e1-2', source: '1', target: '2', animated: true },
-  { id: 'e2-3', source: '2', target: '3', animated: true },
-];
+const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
 
-const App = () => {
-  const [nodes, setNodes] = useState(initialNodes);
-  const [edges, setEdges] = useState(initialEdges);
+function App() {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
-  const onNodesChange = (changes) => setNodes((nds) => ReactFlow.updateNodes(nds, changes));
-  const onEdgesChange = (changes) => setEdges((eds) => ReactFlow.updateEdges(eds, changes));
   const onConnect = (params) => setEdges((eds) => addEdge(params, eds));
 
   return (
-    <ReactFlowProvider>
-      <div style={{ height: '100vh' }}>
-        <ReactFlow
-          nodes={nodes}
-          edges={edges}
-          onNodesChange={onNodesChange}
-          onEdgesChange={onEdgesChange}
-          onConnect={onConnect}
-          fitView
-        >
-          <MiniMap />
-          <Controls />
-          <Background />
-        </ReactFlow>
-      </div>
-    </ReactFlowProvider>
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        fitView
+      >
+        <MiniMap />
+        <Controls />
+        <Background color="#aaa" gap={16} />
+      </ReactFlow>
+    </div>
   );
-};
+}
 
 export default App;
-
